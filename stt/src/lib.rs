162c4a7ad606145c2d546bcbe1951a37118ba70d
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-04-25 21:17:12 -0400

    stt handlers

---
+++ stt/src/lib.rs
@@ -0,0 +1,37 @@
+use tracing::instrument;
+use whisper_rs::{
+    FullParams, SamplingStrategy, WhisperContext, WhisperContextParameters, WhisperState,
+};
+
+#[instrument]
+pub fn create_stt_model(path: String) -> Result<WhisperContext, String> {
+    let mut config = WhisperContextParameters::new();
+    config.use_gpu(true).flash_attn(true).gpu_device(0);
+    match WhisperContext::new_with_params(&path, config) {
+        Ok(context) => Ok(context),
+        Err(err) => Err(format!("Failed to load model: {}", err)),
+    }
+}
+
+#[instrument]
+fn run_stt_model(mut state: WhisperState) -> Result<String, String> {
+    let strategy = SamplingStrategy::Greedy { best_of: 1 };
+    let mut params = FullParams::new(strategy);
+    params.set_print_progress(false);
+    params.set_print_timestamps(false);
+    let result = state.full(params, &[0.0; 10]);
+    match result {
+        Err(err) => Err(format!("Failed to transcribe: {}", err)),
+        Ok(_) => {
+            if let Ok(n) = state.full_n_segments() {
+                let texts = (0..n).map(|i| {
+                    let segment = state.full_get_segment_text(i).unwrap_or(String::from(""));
+                    String::from(segment.trim())
+                });
+                Ok(texts.fold(String::from(""), |acc, s| format!("{acc} {s}")))
+            } else {
+                Err(String::from("Failed to fetch segments"))
+            }
+        }
+    }
+}
--- stt/src/main.rs
+++ stt/src/main.rs
@@ -1,67 +1,21 @@
 use dotenv::dotenv;
-use tracing::instrument;
-use whisper_rs::{
-    FullParams, SamplingStrategy, WhisperContext, WhisperContextParameters,
-    convert_integer_to_float_audio, convert_stereo_to_mono_audio, install_logging_hooks,
-};
-
-#[instrument]
-fn load_model(path: String) -> Result<WhisperContext, String> {
-    let mut config = WhisperContextParameters::new();
-    config.use_gpu(true).flash_attn(true).gpu_device(0);
-    match WhisperContext::new_with_params(&path, config) {
-        Ok(context) => Ok(context),
-        Err(err) => Err(format!("Failed to load model: {}", err)),
-    }
-}
+use stt::create_stt_model;
 
 fn main() {
     dotenv().ok();
-    install_logging_hooks();
+    whisper_rs::install_logging_hooks();
 
-    let model_path = std::env::var("STT_MODEL_PATH").unwrap();
-    let wav_path = std::env::var("STT_SAMPLE_PATH").unwrap();
-    let language = "en";
-
-    let samples: Vec<i16> = hound::WavReader::open(wav_path)
-        .unwrap()
-        .into_samples::<i16>()
-        .map(|x| x.unwrap())
-        .collect();
-
-    let ctx = load_model(model_path).expect("Failed to load model");
-    let mut state = ctx.create_state().expect("Failed to create state");
-
-    let mut params = FullParams::new(SamplingStrategy::Greedy { best_of: 1 });
-    params.set_language(Some(&language));
-    params.set_print_special(false);
-    params.set_print_progress(false);
-    params.set_print_realtime(false);
-    params.set_print_timestamps(false);
-
-    let mut inter_samples = vec![Default::default(); samples.len()];
-    convert_integer_to_float_audio(&samples, &mut inter_samples)
-        .expect("Failed to convert audio data");
-    let samples =
-        convert_stereo_to_mono_audio(&inter_samples).expect("failed to convert audio data");
-
-    state
-        .full(params, &samples[..])
-        .expect("Failed to run model");
-    let num_segments = state
-        .full_n_segments()
-        .expect("Failed to get number of segments");
-
-    for i in 0..num_segments {
-        let segment = state
-            .full_get_segment_text(i)
-            .expect("failed to get segment");
-        let start_timestamp = state
-            .full_get_segment_t0(i)
-            .expect("failed to get segment start timestamp");
-        let end_timestamp = state
-            .full_get_segment_t1(i)
-            .expect("failed to get segment end timestamp");
-        println!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
-    }
+    let _ctx = match std::env::var("STT_MODEL_PATH") {
+        Ok(path) => match create_stt_model(path) {
+            Ok(context) => context,
+            Err(err) => {
+                eprint!("Failed to load model into memory: {}", err);
+                std::process::exit(1);
+            }
+        },
+        Err(err) => {
+            eprint!("Missing STT_MODEL_PATH environment variable: {}", err);
+            std::process::exit(1);
+        }
+    };
 }
